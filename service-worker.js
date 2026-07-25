bb4328ae61e47fdcd8bb8a1e0b94ef848b1fa695
Anshika Pathak <anshikapathak9026@gmail.com>
2026-07-25 14:51:48 +0530

    Added the installation feature from the chorme

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "calculator-v1";
+
+const urls = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urls))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
